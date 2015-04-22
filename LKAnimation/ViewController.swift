e6693e54fc09ad58813a2fbbb60f9f643e28a2a2
liukun <iexpos@163.com>
2015-04-22 15:16:39 +0800

    初始化

---
+++ LKAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LKAnimation
+//
+//  Created by liukun on 15/4/22.
+//  Copyright (c) 2015年 liukun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
